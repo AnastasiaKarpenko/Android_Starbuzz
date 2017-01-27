dcfae19488730cb7b79947901661d613c48e304f
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-01-27 17:37:52 +0200

    edit main activity

--- app/src/main/java/ws/tilda/anastasia/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/ws/tilda/anastasia/starbuzz/DrinkCategoryActivity.java
@@ -3,11 +3,11 @@ package ws.tilda.anastasia.starbuzz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivityTopLevelActivity extends AppCompatActivity {
+public class DrinkCategoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_top_level);
+        setContentView(R.layout.activity_drink_category);
     }
 }
