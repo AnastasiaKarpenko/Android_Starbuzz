89ef39024a424ff60f846ec1c6f166e1872ebf6c
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-01-27 16:48:27 +0200

    Initial commit

---
+++ app/src/main/java/ws/tilda/anastasia/starbuzz/ActivityTopLevelActivity.java
@@ -0,0 +1,13 @@
+package ws.tilda.anastasia.starbuzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+    }
+}
